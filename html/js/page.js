387d9253a00284b2d5e31071b53871093f78b402
tetraloba <four.leaves.siva7342@gmail.com>
2024-09-24 16:07:06 +0900

    test_Post_encodeURL

--- html/js/page.js
+++ html/js/page.js
@@ -17,25 +17,11 @@ function post_content(){
     let index = 0;
     for (line of text_area.children) {
         const data = document.createElement('input');
-        data.value = line.textContent + '\r\n';
+        data.value = encodeURI(line.textContent + '\n');
         data.name = 'lines[' + index + ']';
         form.appendChild(data);
         index++;
     }
-
-    /* debug */
-    let debug_data1 = document.createElement('input');
-    debug_data1.value = '1行目の文字列です';
-    debug_data1.name = 'debug_lines[0]';
-    form.appendChild(debug_data1);
-    let debug_data2 = document.createElement('input');
-    debug_data2.value = '2行目の文字列です';
-    debug_data2.name = 'debug_lines[1]';
-    form.appendChild(debug_data2);
-    let debug_data3 = document.createElement('input');
-    debug_data3.value = '3行目の文字列です';
-    debug_data3.name = 'debug_lines[2]';
-    form.appendChild(debug_data3);
 
     document.body.appendChild(form); // documentに紐づけないとエラーが出る "Form submission canceled because the form is not connected"
 
