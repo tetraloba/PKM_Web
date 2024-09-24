b26c32adb839cea1f7e93a5bacf02eaa85b6dd42
tetraloba <four.leaves.siva7342@gmail.com>
2024-09-24 14:00:36 +0900

    test_php_table

--- html/index.php
+++ html/index.php
@@ -1,24 +1,33 @@
 <?php
+require("html_subr.php");
 $dir_data = './data';
+
+open_html("PKM_Web");
 
 $contents = glob("{$dir_data}/*");
 ?>
-<html>
-    <head>
-        
-    </head>
-    <body>
 
+<table border="1">
 <?php
+tbl_line(['タイトル', '日時', '本文プレビュー']);
 foreach ($contents as $content) {
     // echo $content;
     $lines = file($content);
-    foreach ($lines as $line) {
-        echo $line.' | ';
-    }
-    echo '<br>';
+    tbl_line([
+        pathinfo($content, PATHINFO_BASENAME),
+        pathinfo($content, PATHINFO_FILENAME),
+        $lines[0],
+        $lines[1],
+        $lines[2],
+    ]);
+    // foreach ($lines as $line) {
+    //     echo $line.' | ';
+    // }
+    // echo '<br>';
 }
 ?>
+</table><br>
 
-    </body>
-</html>
+<?php
+close_html();
+?>
